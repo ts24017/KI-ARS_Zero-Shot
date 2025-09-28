32039f3acbe252436cf4e17904c81cc168186ecd
Tony Steinbach <tonysteinbach@MacBook-Pro-von-Tony.fritz.box>
2025-09-29 00:46:30 +0200

    Initial commit

---
+++ src/main/java/com/example/kiars_zeroshot/KiArsZeroShotApplication.java
@@ -0,0 +1,13 @@
+package com.example.kiars_zeroshot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiArsZeroShotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KiArsZeroShotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kiars_zeroshot/KiArsZeroShotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kiars_zeroshot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KiArsZeroShotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
